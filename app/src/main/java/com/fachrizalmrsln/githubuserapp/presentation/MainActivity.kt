6e2949dcb10806c7a5d84dbdad533c0360d313b5
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-17 13:36:16 +0800

    moved MainActivity.kt class to its package

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/MainActivity.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.fachrizalmrsln.githubuserapp
+package com.fachrizalmrsln.githubuserapp.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fachrizalmrsln.githubuserapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

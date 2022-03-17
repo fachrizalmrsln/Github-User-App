50ad7ea019f8227d9f2680ca9a528e59acebdad2
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-17 14:13:36 +0800

    creating ConstantValues.kt

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/splash_screen/SplashScreenViewModel.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/splash_screen/SplashScreenViewModel.kt
@@ -2,6 +2,7 @@ package com.fachrizalmrsln.githubuserapp.presentation.splash_screen
 
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ViewModel
+import com.fachrizalmrsln.githubuserapp.utils.SPLASH_SCREEN_DURATION
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
@@ -11,9 +12,8 @@ import javax.inject.Inject
 @HiltViewModel
 class SplashScreenViewModel @Inject constructor() : ViewModel(), LifecycleObserver {
 
-    private val mSplashScreenDuration = 3000L
     val mLaunchToHome: Flow<Boolean> = flow {
-        delay(mSplashScreenDuration)
+        delay(SPLASH_SCREEN_DURATION)
         emit(true)
     }
 
---
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/ConstantValues.kt
@@ -0,0 +1,3 @@
+package com.fachrizalmrsln.githubuserapp.utils
+
+const val SPLASH_SCREEN_DURATION = 3000L

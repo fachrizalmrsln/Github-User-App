ec0769d74466ece405c4899137c735b9419532cd
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-17 19:47:20 +0800

    refactor SplashScreen.kt using new abstraction

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/splash_screen/SplashScreen.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/splash_screen/SplashScreen.kt
@@ -2,28 +2,37 @@ package com.fachrizalmrsln.githubuserapp.presentation.splash_screen
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import com.fachrizalmrsln.githubuserapp.R
-import com.fachrizalmrsln.githubuserapp.base.BaseClass
+import android.view.LayoutInflater
+import com.fachrizalmrsln.githubuserapp.base.BaseActivity
+import com.fachrizalmrsln.githubuserapp.databinding.ActivitySplashScreenBinding
 import com.fachrizalmrsln.githubuserapp.navigation.navigateToHome
 import com.fachrizalmrsln.githubuserapp.utils.constant.SPLASH_SCREEN_DURATION
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
-class SplashScreen : BaseClass() {
+class SplashScreen : BaseActivity<ActivitySplashScreenBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
 
         delayingScreen()
     }
 
+    override val mBindingInflater: (LayoutInflater) -> ActivitySplashScreenBinding
+        get() = ActivitySplashScreenBinding::inflate
+
     private fun delayingScreen() = mScopeMain.launch {
-        delay(SPLASH_SCREEN_DURATION)
-        navigateToHome(clearStack = true)
+    }
+
+    override fun activityScope(scope: CoroutineScope) {
+        scope.launch {
+            delay(SPLASH_SCREEN_DURATION)
+            navigateToHome(clearStack = true)
+        }
     }
 
 }

6532484fefa6cfc9ed9558e20164a805d9200005
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 15:32:43 +0800

    refactoring logic fetch data in HomePageViewModel.kt

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/home_page/HomePageViewModel.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/presentation/home_page/HomePageViewModel.kt
@@ -1,12 +1,15 @@
 package com.fachrizalmrsln.githubuserapp.presentation.home_page
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.fachrizalmrsln.githubuserapp.base.BaseViewModel
 import com.fachrizalmrsln.githubuserapp.data.local.repository.IRemoteRepository
 import com.fachrizalmrsln.githubuserapp.model.SearchItemModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.cancellable
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,13 +17,23 @@ class HomePageViewModel @Inject constructor(
     private val repository: IRemoteRepository
 ) : BaseViewModel() {
 
-    suspend fun searchUser(query: String): Flow<List<SearchItemModel>> {
+    private var _mSearchResults = MutableLiveData<List<SearchItemModel>>()
+    val mSearchResults: LiveData<List<SearchItemModel>>
+        get() = _mSearchResults
+
+    suspend fun searchUser(query: String) {
         showLoading()
-        return repository.getSearchUser(query)
-            .catch {
-                _messageToUI.value = it.message
-            }
-            .onCompletion { hideLoading() }
+        restartJob(INITIAL)
+        launch {
+            repository.getSearchUser(query)
+                .catch {
+                    _messageToUI.value = it.message
+                    restartJob()
+                }
+                .onCompletion { hideLoading() }
+                .cancellable()
+                .collect { _mSearchResults.value = it }
+        }
     }
 
 }

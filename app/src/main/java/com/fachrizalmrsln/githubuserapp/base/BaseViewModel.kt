e898c9462c6b5a1a665b4e4c731306c34e9c2a0b
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 10:09:00 +0800

    remove unused code

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/base/BaseViewModel.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/base/BaseViewModel.kt
@@ -3,7 +3,6 @@ package com.fachrizalmrsln.githubuserapp.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.fachrizalmrsln.githubuserapp.model.Message
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
@@ -14,13 +13,10 @@ abstract class BaseViewModel : ViewModel(), CoroutineScope {
     private val _loadingStatus = MutableLiveData<Boolean>()
     val loadingStatus: LiveData<Boolean> = _loadingStatus
 
-    protected val _stringMessage = MutableLiveData<String>()
-    val stringMessage: LiveData<String> = _stringMessage
+    protected val _messageToUI = MutableLiveData<String>()
+    val messageToUI: LiveData<String> = _messageToUI
 
-    protected val _message = MutableLiveData<Message>()
-    val message: LiveData<Message> = _message
-
-    protected val job = Job()
+    private val job = Job()
     override val coroutineContext: CoroutineContext
         get() = Dispatchers.Main + job
 
@@ -37,20 +33,6 @@ abstract class BaseViewModel : ViewModel(), CoroutineScope {
         if (_loadingStatus.value == true) {
             _loadingStatus.value = false
         }
-    }
-
-    protected suspend fun <T> BaseNetworkResponse<T>.onResult(result: suspend (T?, code: Int, message: String) -> Unit) {
-        when (this) {
-            is BaseNetworkResponse.Ok -> result.invoke(value, 200, "")
-            is BaseNetworkResponse.Error -> _message.value = Message.Dialog(response.message)
-            is BaseNetworkResponse.Exception -> _message.value = Message.Dialog(exception.message)
-            is BaseNetworkResponse.ErrorCode -> errorResultCodeHandle(message)
-            is BaseNetworkResponse.NoNetwork -> _message.value = Message.Dialog(exception.message)
-        }
-    }
-
-    protected fun errorResultCodeHandle(message: String) {
-        _message.value = Message.Dialog(message)
     }
 
 }

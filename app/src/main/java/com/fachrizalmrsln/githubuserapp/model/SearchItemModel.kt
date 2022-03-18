ed7dae46154463720655eb453f98bed928fd2438
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 21:03:12 +0800

    make SearchItemModel.kt parcelable

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/model/SearchItemModel.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/model/SearchItemModel.kt
@@ -1,5 +1,9 @@
 package com.fachrizalmrsln.githubuserapp.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class SearchItemModel(
     var id: Int,
     var avatar_url: String,
@@ -8,4 +12,6 @@ data class SearchItemModel(
     var bio: String,
     var location: String,
     var email: String,
-)
+    var follower: String,
+    var following: String
+) : Parcelable

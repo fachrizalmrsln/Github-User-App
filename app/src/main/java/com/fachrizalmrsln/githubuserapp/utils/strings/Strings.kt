e4d0dd1755486b4bbac8e3096216514abb474d07
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 20:59:39 +0800

    add new function in Strings.kt

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/strings/Strings.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/strings/Strings.kt
@@ -3,3 +3,5 @@ package com.fachrizalmrsln.githubuserapp.utils.strings
 fun String?.checkNullOrEmpty() = if (this.isNullOrEmpty()) "-" else this
 
 fun String?.isUserName() = if (this.isNullOrEmpty()) "-" else "@$this"
+
+fun Int?.checkNullOrEmpty() = this?.toString() ?: "0"

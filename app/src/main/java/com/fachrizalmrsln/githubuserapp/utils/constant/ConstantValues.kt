2403e47cabb449f9e3c16992d63cf6bd59a66a6b
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 11:50:39 +0800

    add new ConstantValues.kt variables

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/constant/ConstantValues.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/constant/ConstantValues.kt
@@ -3,3 +3,6 @@ package com.fachrizalmrsln.githubuserapp.utils.constant
 const val BASE_URL = "https://api.github.com/"
 const val AUTHORIZATION = "Authorization"
 const val SPLASH_SCREEN_DURATION = 3000L
+const val VIEW_TYPE_LOADING = 0
+const val VIEW_TYPE_ITEM = 1
+const val VIEW_TYPE_NO_SPACE = 2

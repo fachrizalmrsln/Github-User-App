9bfdb22ac5be94a07e9dd291e11f0133490de1a4
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 17:54:07 +0800

    refactoring string utils isUserName logic to handle nullable strings

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/strings/Strings.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/strings/Strings.kt
@@ -2,4 +2,4 @@ package com.fachrizalmrsln.githubuserapp.utils.strings
 
 fun String?.checkNullOrEmpty() = if (this.isNullOrEmpty()) "-" else this
 
-fun String.isUserName() = "@$this"
+fun String?.isUserName() = if (this.isNullOrEmpty()) "-" else "@$this"

3649954c33c450c48334879d5e06823d64f3f780
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-18 09:19:23 +0800

    adding new const value and new Strings util class

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/constant/ConstantValues.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/constant/ConstantValues.kt
@@ -1,4 +1,5 @@
 package com.fachrizalmrsln.githubuserapp.utils.constant
 
 const val BASE_URL = "https://api.github.com/"
+const val AUTHORIZATION = "Authorization"
 const val SPLASH_SCREEN_DURATION = 3000L

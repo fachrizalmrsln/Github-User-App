c2bd4733e63102d6a17f4f89745cff032a3cb692
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 00:14:46 +0800

    set entity class

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/model/UserRepositories.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/model/UserRepositories.kt
@@ -1,11 +1,25 @@
 package com.fachrizalmrsln.githubuserapp.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_repositories_table")
 data class UserRepositories(
+    @PrimaryKey
+    @ColumnInfo(name = "id_repositories_repositories")
     val id: Int,
+    @ColumnInfo(name = "image_user_repositories")
     var avatar_url: String?,
+    @ColumnInfo(name = "name_repositories_repositories")
     val name: String?,
+    @ColumnInfo(name = "description_repositories_repositories")
     val description: String?,
+    @ColumnInfo(name = "star_repositories_repositories")
     val stargazers_count: Int?,
+    @ColumnInfo(name = "update_repositories_repositories")
     var updated_at: String?,
+    @Embedded
     val owner: UserModel
 )

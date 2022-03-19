cc84a5037b805c384e94b10bde1add9696889ae2
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 00:16:32 +0800

    setup di for local data module

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/app/GitHubUserApp.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/app/GitHubUserApp.kt
@@ -1,12 +1,19 @@
 package com.fachrizalmrsln.githubuserapp.app
 
 import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import com.fachrizalmrsln.githubuserapp.data.local.database.DatabaseInstance
+import com.fachrizalmrsln.githubuserapp.data.local.repository.ILocalRepository
+import com.fachrizalmrsln.githubuserapp.data.local.repository.LocalRepository
 import com.fachrizalmrsln.githubuserapp.data.remote.repository.IRemoteRepository
 import com.fachrizalmrsln.githubuserapp.data.remote.repository.RemoteRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -19,8 +26,34 @@ abstract class GitHubUserAppModule {
 
     @Binds
     @Singleton
-    abstract fun initializeRepository(
+    abstract fun bindsRemoteRepository(
         remoteRepository: RemoteRepository
     ): IRemoteRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsLocalRepository(
+        localRepository: LocalRepository
+    ): ILocalRepository
+
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object LocalDataModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        DatabaseInstance::class.java,
+        "GitHubUserApp"
+    ).build()
+
+    @Provides
+    @Singleton
+    fun provideDatabaseInstance(db: DatabaseInstance) = db.databaseObject()
 
 }

d85269c9c5a8126194086808b81a5e2e31388c05
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-19 12:01:54 +0800

    adding test case for isUserName()

--- app/src/test/java/com/fachrizalmrsln/githubuserapp/utils/strings/StringsUnitTest.kt
+++ app/src/test/java/com/fachrizalmrsln/githubuserapp/utils/strings/StringsUnitTest.kt
@@ -32,4 +32,30 @@ class StringsUnitTest {
         assertNotEquals(mExpectedData2, mResult2)
     }
 
+    @Test
+    fun isUserName_isCorrect() {
+        val mGivenData1 = "fachrizalmrsln"
+        val mExpectedData1 = "@fachrizalmrsln"
+        val mResult1 = mGivenData1.isUserName()
+        assertEquals(mExpectedData1, mResult1)
+
+        val mGivenData2: String? = null
+        val mExpectedData2 = "-"
+        val mResult2 = mGivenData2.isUserName()
+        assertEquals(mExpectedData2, mResult2)
+    }
+
+    @Test
+    fun isUserName_isInCorrect() {
+        val mGivenData1 = "fachrizalmrsln"
+        val mExpectedData1 = "fachrizalmrsln"
+        val mResult1 = mGivenData1.isUserName()
+        assertNotEquals(mExpectedData1, mResult1)
+
+        val mGivenData2: String? = null
+        val mExpectedData2 = null
+        val mResult2 = mGivenData2.isUserName()
+        assertNotEquals(mExpectedData2, mResult2)
+    }
+
 }

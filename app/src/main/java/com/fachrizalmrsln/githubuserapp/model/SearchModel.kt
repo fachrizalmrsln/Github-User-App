5ac48b36ff461e4b912af394b0bf92405e8e8daf
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-19 15:30:07 +0800

    remove unused fields

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/model/SearchModel.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/model/SearchModel.kt
@@ -1,7 +1,3 @@
 package com.fachrizalmrsln.githubuserapp.model
 
-data class SearchModel(
-    val incomplete_results: Boolean,
-    val items: List<SearchItemModel>,
-    val total_count: Int
-)
+data class SearchModel(val items: List<SearchItemModel>)

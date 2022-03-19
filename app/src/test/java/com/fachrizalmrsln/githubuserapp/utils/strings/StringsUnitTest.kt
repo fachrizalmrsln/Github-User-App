c2e3d7686a5ee9a8450c67b2eb0bcfe5aef2b777
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-19 11:59:26 +0800

    add test case for checkNullOrEmpty()

--- app/src/test/java/com/fachrizalmrsln/githubuserapp/utils/strings/StringsUnitTest.kt
+++ app/src/test/java/com/fachrizalmrsln/githubuserapp/utils/strings/StringsUnitTest.kt
@@ -1,6 +1,35 @@
 package com.fachrizalmrsln.githubuserapp.utils.strings
 
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Test
 
 class StringsUnitTest {
+
+    @Test
+    fun checkNullOrEmpty_isCorrect() {
+        val mGivenData1 = "This is test"
+        val mExpectedData1 = "This is test"
+        val mResult1 = mGivenData1.checkNullOrEmpty()
+        assertEquals(mExpectedData1, mResult1)
+
+        val mGivenData2: String? = null
+        val mExpectedData2 = "-"
+        val mResult2 = mGivenData2.checkNullOrEmpty()
+        assertEquals(mExpectedData2, mResult2)
+    }
+
+    @Test
+    fun checkNullOrEmpty_isInCorrect() {
+        val mGivenData1 = "This is test"
+        val mExpectedData1 = 23
+        val mResult1 = mGivenData1.checkNullOrEmpty()
+        assertNotEquals(mExpectedData1, mResult1)
+
+        val mGivenData2: String? = null
+        val mExpectedData2 = null
+        val mResult2 = mGivenData2.checkNullOrEmpty()
+        assertNotEquals(mExpectedData2, mResult2)
+    }
 
 }

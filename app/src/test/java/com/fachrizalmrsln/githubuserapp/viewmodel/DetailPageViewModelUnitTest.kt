68f83a820da04fa2eab8e23ebc1e8f874e3f437d
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-19 19:31:56 +0800

    add fail test case

--- app/src/test/java/com/fachrizalmrsln/githubuserapp/viewmodel/DetailPageViewModelUnitTest.kt
+++ app/src/test/java/com/fachrizalmrsln/githubuserapp/viewmodel/DetailPageViewModelUnitTest.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
@@ -81,7 +82,42 @@ class DetailPageViewModelUnitTest {
             Mockito.verify(mObserver).onChanged(captor.capture())
             assertEquals(0, captor.value.size)
             assertEquals(223750327, captor.value[0].id)
-            assertEquals(mDataExpected, captor.value[0])
+        }
+    }
+
+    @Test
+    fun getSearchUser_isInCorrect() {
+        val mQuery = "fachrizalmrsln"
+        val mDataExpected = UserRepositories(
+            id = 111111,
+            avatar_url = "https://avatars.githubusercontent.com/u/28893080?v=4",
+            name = "Beautiful-Place-App",
+            description = "Application for certification in AndroidPemula class from dicoding.",
+            stargazers_count = 0,
+            updated_at = "2019-12-15T09:25:37Z",
+            owner = UserModel(
+                avatar_url = "https://avatars.githubusercontent.com/u/28893080?v=4",
+                bio = "Learner & Code writer",
+                location = "South Jakarta, Jakarta, Indonesia",
+                email = "fachrizalmrsln@gmail.com",
+                following = 26,
+                followers = 3,
+                name = "Fachrizal Mursalin"
+            )
+        )
+        runBlocking {
+            val flow = flow { emit(mResponse) }
+
+            `when`(mRepository.getUserRepositories(mQuery)).thenReturn(flow)
+            `when`(mResponse[0]).thenReturn(mDataExpected)
+
+            mViewModel.getUserRepositories(mQuery)
+            val liveData = mViewModel.mUserRepositories
+            liveData.observeForever(mObserver)
+
+            Mockito.verify(mObserver).onChanged(captor.capture())
+            assertNotEquals(1, captor.value.size)
+            assertNotEquals(223750327, captor.value[0].id)
         }
     }
 

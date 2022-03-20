f83050befe463673642c163bd74e75977cbb5192
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 18:47:12 +0800

    handling repositories set and get data from ILocalSource.kt

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/source/ILocalSource.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/source/ILocalSource.kt
@@ -14,13 +14,13 @@ interface ILocalSource {
     suspend fun saveSearchDataHistory(dataSearch: List<SearchItemModel>)
 
     @Insert(onConflict = IGNORE)
-    suspend fun saveRepositories(dataRepositories: UserRepositories)
+    suspend fun saveRepositories(dataRepositories: List<UserRepositories>)
 
     @Query("SELECT * FROM search_history_table WHERE username_user_search LIKE '%'||:query||'%' " +
             "OR name_user_search LIKE '%'||:query||'%'")
     suspend fun getSearchDataHistory(query: String): List<SearchItemModel>
 
-    @Query("SELECT * FROM user_repositories_table")
-    suspend fun getRepositories(): List<UserRepositories>
+    @Query("SELECT * FROM user_repositories_table WHERE user_name_user_user = :userName")
+    suspend fun getRepositories(userName: String): List<UserRepositories>
 
 }

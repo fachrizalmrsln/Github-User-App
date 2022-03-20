41f1a5884a3fb9cdcec6f58e0f4fe9bec7311ef4
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 21:29:25 +0800

    add save and get search query

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/repository/ILocalRepository.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/repository/ILocalRepository.kt
@@ -1,13 +1,21 @@
 package com.fachrizalmrsln.githubuserapp.data.local.repository
 
 import com.fachrizalmrsln.githubuserapp.model.SearchItemModel
+import com.fachrizalmrsln.githubuserapp.model.SearchQueryModel
+import com.fachrizalmrsln.githubuserapp.model.SearchRoomResultModel
 import com.fachrizalmrsln.githubuserapp.model.UserRepositoriesModel
 
 interface ILocalRepository {
 
+    suspend fun saveSearchQuery(dataQuery: SearchQueryModel)
+
     suspend fun saveSearchDataHistory(dataSearch: List<SearchItemModel>)
 
     suspend fun saveRepositories(dataRepositories: List<UserRepositoriesModel>)
+
+    suspend fun getSearchQuery(query: String): SearchQueryModel?
+
+    suspend fun getSearchHistory(): List<SearchRoomResultModel>
 
     suspend fun getSearchDataHistory(query: String): List<SearchItemModel>
 
--- app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/repository/LocalRepository.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/repository/LocalRepository.kt
@@ -2,27 +2,57 @@ package com.fachrizalmrsln.githubuserapp.data.local.repository
 
 import com.fachrizalmrsln.githubuserapp.data.local.source.ILocalSource
 import com.fachrizalmrsln.githubuserapp.model.SearchItemModel
+import com.fachrizalmrsln.githubuserapp.model.SearchQueryModel
+import com.fachrizalmrsln.githubuserapp.model.SearchRoomResultModel
 import com.fachrizalmrsln.githubuserapp.model.UserRepositoriesModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class LocalRepository @Inject constructor(
     private val mDao: ILocalSource
 ) : ILocalRepository {
 
+    override suspend fun saveSearchQuery(dataQuery: SearchQueryModel) {
+        withContext(Dispatchers.IO) {
+            mDao.saveSearchQuery(dataQuery)
+        }
+    }
+
     override suspend fun saveSearchDataHistory(dataSearch: List<SearchItemModel>) {
-        mDao.saveSearchDataHistory(dataSearch)
+        withContext(Dispatchers.IO) {
+            mDao.saveSearchDataHistory(dataSearch)
+        }
     }
 
     override suspend fun saveRepositories(dataRepositories: List<UserRepositoriesModel>) {
-        mDao.saveRepositories(dataRepositories)
+        withContext(Dispatchers.IO) {
+            mDao.saveRepositories(dataRepositories)
+        }
+    }
+
+    override suspend fun getSearchQuery(query: String): SearchQueryModel? {
+        return withContext(Dispatchers.IO) {
+            mDao.getSearchQuery(query)
+        }
+    }
+
+    override suspend fun getSearchHistory(): List<SearchRoomResultModel> {
+        return withContext(Dispatchers.IO) {
+            mDao.getSearchHistory()
+        }
     }
 
     override suspend fun getSearchDataHistory(query: String): List<SearchItemModel> {
-        return mDao.getSearchDataHistory(query)
+        return withContext(Dispatchers.IO) {
+            mDao.getSearchDataHistory(query)
+        }
     }
 
     override suspend fun getRepositories(userName: String): List<UserRepositoriesModel> {
-        return mDao.getRepositories(userName)
+        return withContext(Dispatchers.IO) {
+            mDao.getRepositories(userName)
+        }
     }
 
 }

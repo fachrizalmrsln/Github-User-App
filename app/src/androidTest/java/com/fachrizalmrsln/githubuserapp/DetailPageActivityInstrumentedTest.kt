7ad7188b046aa745f404b5106fa400c324877594
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 22:19:43 +0800

    write test case for caching user detail

--- app/src/androidTest/java/com/fachrizalmrsln/githubuserapp/DetailPageActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/fachrizalmrsln/githubuserapp/DetailPageActivityInstrumentedTest.kt
@@ -1,15 +1,19 @@
 package com.fachrizalmrsln.githubuserapp
 
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
-import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.fachrizalmrsln.githubuserapp.data.local.source.DatabaseInstance
+import com.fachrizalmrsln.githubuserapp.data.local.source.ILocalSource
 import com.fachrizalmrsln.githubuserapp.presentation.splash_screen.SplashScreen
 import com.fachrizalmrsln.githubuserapp.utils.constant.SPLASH_SCREEN_DURATION
 import kotlinx.coroutines.delay
@@ -24,6 +28,9 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class DetailPageActivityInstrumentedTest {
 
+    private lateinit var userDao: ILocalSource
+    private lateinit var db: DatabaseInstance
+
     @get: Rule
     var mActivityRule = ActivityTestRule(SplashScreen::class.java)
 
@@ -32,6 +39,10 @@ class DetailPageActivityInstrumentedTest {
     @Before
     fun setup() {
         mMockWebServer.start(8080)
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, DatabaseInstance::class.java).build()
+        userDao = db.databaseObject()
     }
 
     @Test
@@ -55,6 +66,37 @@ class DetailPageActivityInstrumentedTest {
             .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(3, click()))
     }
 
+    @Test
+    fun actionSearchAndSelectItemBackAndReselectThenScrollTheRepositoryList() {
+        waitForSplashScreen()
+
+        onView(withId(R.id.et_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(click())
+            .perform(typeText("Fachrizalmrsln"))
+            .perform(pressImeActionButton())
+
+        onView(withId(R.id.rv_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.rv_repositories))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(19))
+
+        onView(isRoot())
+            .check(matches(isCompletelyDisplayed()))
+            .perform(pressBack())
+
+        onView(withId(R.id.rv_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+
+        onView(withId(R.id.rv_repositories))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(scrollToPosition<RecyclerView.ViewHolder>(19))
+    }
+
     private fun waitForSplashScreen() = runBlocking {
         delay(SPLASH_SCREEN_DURATION)
     }
@@ -62,6 +104,7 @@ class DetailPageActivityInstrumentedTest {
     @After
     fun teardown() {
         mMockWebServer.shutdown()
+        db.close()
     }
 
 }

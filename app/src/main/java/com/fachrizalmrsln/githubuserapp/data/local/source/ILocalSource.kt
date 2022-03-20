2a13ce5e9917c17f3a539d96c755ab24e9310ca2
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 18:50:54 +0800

    change conflict strategy to replace

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/source/ILocalSource.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/source/ILocalSource.kt
@@ -2,7 +2,7 @@ package com.fachrizalmrsln.githubuserapp.data.local.source
 
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.fachrizalmrsln.githubuserapp.model.SearchItemModel
 import com.fachrizalmrsln.githubuserapp.model.UserRepositories
@@ -10,14 +10,14 @@ import com.fachrizalmrsln.githubuserapp.model.UserRepositories
 @Dao
 interface ILocalSource {
 
-    @Insert(onConflict = IGNORE)
+    @Insert(onConflict = REPLACE)
     suspend fun saveSearchDataHistory(dataSearch: List<SearchItemModel>)
 
-    @Insert(onConflict = IGNORE)
+    @Insert(onConflict = REPLACE)
     suspend fun saveRepositories(dataRepositories: List<UserRepositories>)
 
     @Query("SELECT * FROM search_history_table WHERE username_user_search LIKE '%'||:query||'%' " +
-            "OR name_user_search LIKE '%'||:query||'%'")
+                "OR name_user_search LIKE '%'||:query||'%'")
     suspend fun getSearchDataHistory(query: String): List<SearchItemModel>
 
     @Query("SELECT * FROM user_repositories_table WHERE user_name_user_user = :userName")

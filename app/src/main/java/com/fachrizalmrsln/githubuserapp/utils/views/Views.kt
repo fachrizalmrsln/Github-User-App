6b693d15bbd7c6256cfc5a5069185a37a510342b
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 23:05:59 +0800

    create new views utils

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/views/Views.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/utils/views/Views.kt
@@ -1,6 +1,8 @@
 package com.fachrizalmrsln.githubuserapp.utils.views
 
+import android.app.Activity
 import android.view.View
+import androidx.core.content.ContextCompat
 
 fun View.visible() {
     this.visibility = View.VISIBLE
@@ -9,3 +11,5 @@ fun View.visible() {
 fun View.gone() {
     this.visibility = View.GONE
 }
+
+fun Activity.loadDrawable(drawableID: Int) = ContextCompat.getDrawable(this, drawableID)

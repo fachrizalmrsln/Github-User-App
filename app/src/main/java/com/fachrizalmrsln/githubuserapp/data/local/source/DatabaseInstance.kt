8c39e3c6a3e3cccc31c00fdb4200ae3e3e8ef28a
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 21:28:17 +0800

    add new entity

--- app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/source/DatabaseInstance.kt
+++ app/src/main/java/com/fachrizalmrsln/githubuserapp/data/local/source/DatabaseInstance.kt
@@ -3,9 +3,15 @@ package com.fachrizalmrsln.githubuserapp.data.local.source
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.fachrizalmrsln.githubuserapp.model.SearchItemModel
+import com.fachrizalmrsln.githubuserapp.model.SearchQueryModel
 import com.fachrizalmrsln.githubuserapp.model.UserRepositoriesModel
 
-@Database(entities = [SearchItemModel::class, UserRepositoriesModel::class], version = 1)
+@Database(
+    entities = [
+        SearchItemModel::class,
+        UserRepositoriesModel::class,
+        SearchQueryModel::class], version = 1
+)
 abstract class DatabaseInstance : RoomDatabase() {
 
     abstract fun databaseObject(): ILocalSource

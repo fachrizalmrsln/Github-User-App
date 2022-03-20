4085434b78c2da30e99a97e1625c015288e1cfc9
Fachrizal <fachrizalmrsln@gmail.com>
2022-03-20 22:15:27 +0800

    write test case for caching search history

--- app/src/androidTest/java/com/fachrizalmrsln/githubuserapp/HomePageActivityInstrumentedTest.kt
+++ app/src/androidTest/java/com/fachrizalmrsln/githubuserapp/HomePageActivityInstrumentedTest.kt
@@ -1,6 +1,9 @@
 package com.fachrizalmrsln.githubuserapp
 
+import android.content.Context
 import androidx.recyclerview.widget.RecyclerView
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -10,6 +13,8 @@ import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.rule.ActivityTestRule
+import com.fachrizalmrsln.githubuserapp.data.local.source.DatabaseInstance
+import com.fachrizalmrsln.githubuserapp.data.local.source.ILocalSource
 import com.fachrizalmrsln.githubuserapp.presentation.splash_screen.SplashScreen
 import com.fachrizalmrsln.githubuserapp.utils.constant.SPLASH_SCREEN_DURATION
 import kotlinx.coroutines.delay
@@ -24,6 +29,10 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class HomePageActivityInstrumentedTest {
 
+    private lateinit var userDao: ILocalSource
+    private lateinit var db: DatabaseInstance
+
+
     @get: Rule
     var mActivityRule = ActivityTestRule(SplashScreen::class.java)
 
@@ -32,6 +41,10 @@ class HomePageActivityInstrumentedTest {
     @Before
     fun setup() {
         mMockWebServer.start(8080)
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(
+            context, DatabaseInstance::class.java).build()
+        userDao = db.databaseObject()
     }
 
     @Test
@@ -50,6 +63,35 @@ class HomePageActivityInstrumentedTest {
             .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(3, click()))
     }
 
+    @Test
+    fun actionSearchCachedAndSelectItem() {
+        waitForSplashScreen()
+
+        onView(withId(R.id.et_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(click())
+            .perform(typeText("Fachrizalmrsln"))
+            .perform(pressImeActionButton())
+
+        onView(withId(R.id.et_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(click())
+            .perform(clearText())
+            .perform(typeText("Fachrizal"))
+            .perform(pressImeActionButton())
+
+        onView(withId(R.id.et_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(click())
+            .perform(clearText())
+            .perform(typeText("Fachrizalmrsln"))
+            .perform(pressImeActionButton())
+
+        onView(withId(R.id.rv_search))
+            .check(matches(isCompletelyDisplayed()))
+            .perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+    }
+
     private fun waitForSplashScreen() = runBlocking {
         delay(SPLASH_SCREEN_DURATION)
     }
@@ -57,6 +99,7 @@ class HomePageActivityInstrumentedTest {
     @After
     fun teardown() {
         mMockWebServer.shutdown()
+        db.close()
     }
 
 }
